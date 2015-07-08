72fdc25bbc1323427becad528d54b125879a5ff0
dviveiros <daniel.viveiros@gmail.com>
2015-07-08 19:32:19 -0300

    Cards API, first version

---
+++ api-docs/server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# To open Swagger UI on your browser, just go to
+# http://localhost:8000/swagger-ui/index.html
+
+# runs a local http server to open swagger UI
+python -m SimpleHTTPServer 8000
